96bf78c4f3294e2e49cdd189b433ab13b3fb4d1b
Nicko <nikos.tsagog@gmail.com>
2025-04-03 12:03:06 -0400

    Input ranks and suits

--- src/Game.java
+++ src/Game.java
@@ -25,7 +25,23 @@ public class Game{
         frame.add(panel);
         frame.setVisible(true); 
 
+    }
+}
 
-
+class Deck{
+    static void cards() {
+        String[] suits = {"Hearts", "Diamonds", "Clubs", "Spades"};
+        String[] ranks = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace"};
+        String[] deck = new String[52];
+        int index = 0;
+        for (String suit : suits) {
+            for (String rank : ranks) {
+                deck[index++] = rank + " of " + suit;
+            }
+        }        
+    
+    
+    
+    
     }
 }
