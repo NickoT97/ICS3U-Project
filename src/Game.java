628bd37ea1dc7464679da8288d2468054940e097
Nicko <nikos.tsagog@gmail.com>
2025-04-03 11:39:43 -0400

    Practicing how to implement GUI

---
+++ src/Game.java
@@ -0,0 +1,31 @@
+import java.awt.*;
+import javax.swing.*;
+
+public class Game{
+    public static void main(String [] args){
+
+        JFrame frame = new JFrame("Uno");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
+        frame.setSize(960,540);
+        frame.setLocationRelativeTo(null);
+        frame.setBackground(Color.CYAN);
+
+        ImageIcon background = new ImageIcon(("/Users/nicko/Downloads/PokerTable.jpeg"));
+        Image img = background.getImage();
+
+        JPanel panel = new JPanel(){
+            @Override
+            protected void paintComponent(Graphics g) {
+                super.paintComponent(g);
+                g.drawImage(img, 0, 0, getWidth(), getHeight(), this);
+            }
+        };
+        
+        frame.add(panel);
+        frame.setVisible(true); 
+
+
+
+    }
+}
