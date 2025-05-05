8e34ec77c9406751ed7d4680a4406f1604ef390f
Nicko <nikos.tsagog@gmail.com>
2025-05-05 09:25:06 -0400

    Changed arrays & the user can now draw cards

--- src/Game.java
+++ src/Game.java
@@ -1,6 +1,6 @@
 import java.awt.*;
-import javax.swing.*;
 import java.util.Scanner;
+import javax.swing.*;
 
 public class Game{
     public static void main(String [] args){
@@ -29,11 +29,16 @@ public class Game{
 
 
         System.out.println("Welcome to Uno!");
-        System.out.println("Do you want to play? (yes/no)");
+        System.out.println("Do you want to play? (yes/no)"); // Ask the user if they want to play the game
             String response = scan.nextLine().toLowerCase();
 
         if (response.equals("yes")){
             System.out.println("Great! Let's play!");
+            System.out.println("Handing out 7 cards...");
+
+            System.out.println("Your cards are: ");
+            Deck.cards(); // Call the cards method in the Deck class, and draw 7 cards
+
         }
 
         else if (response.equals("no")) {
@@ -51,24 +56,46 @@ public class Game{
 
 class Deck{
     public static void cards() {
-        String[][] card = new String[4][];
-        card[0] = new String[25]; // Red
-        card[1] = new String[25]; // Blue
-        card[2] = new String[25]; // Green
-        card[3] = new String[8]; // Wild
+        int[][] card = new int[4][];
+        
+        card[0] = new int[25]; // Red
+        
+        for (int i = 0; i < 25; i++){
+            card[0][0] = 4; // Give each card a value of 4, which means it is in the deck
+            }
 
-        int[] location = new int[83]; 
+        card[1] = new int[25]; // Blue
+        
+        for (int i = 0; i < 25; i++){
+            card[1][i] = 4; // Give each card a value of 4, which means it is in the deck
+        }
+
+        card[2] = new int[25]; // Green
+        
+        for (int i = 0; i < 25; i++){
+            card[2][i] = 4; // Give each card a value of 4, which means it is in the deck
+        }
+
+        card[3] = new int[8]; // Wild
+        
+        for (int i = 0; i < 25; i++){
+            card[3][i] = 4; // Give each card a value of 4, which means it is in the deck
+        }
 
         int index1 = 0;
         int index2 = 0;
 
         for (int i = 0; i < 7; i++){
-            index1 = (int) Math.random() * 4;
-            index2 = (int) Math.random() * 25;
+            index1 = (int) (Math.random() * 4); // The first index is the color of the card (red, blue, green, or wild)
+            index2 = (int) (Math.random() * 25); // The second index is the number of the card (0-25)
+            
+            if (index1 == 3) // If the card is wild (index1 == 3), the randomizer will draw from a pile of 8 cards
+                index2 = (int) (Math.random() * 8); // Thw ild card has a pile of 8 cards
+            }
 
             System.out.println(card[index1][index2]);
+
+            card[index1][index2] = 1; // Give the card a value of one, which means it is in the player's hand
         }
-    
-    
+
     }
-}
