9297cb7ea7500aa0109ba6857bba053386a2a8c9
JTUsmar <jamieusmar@crescentschool.org>
2025-05-07 14:04:28 -0400

    Multiplayer + Turns
    Added turns, added multiplayer.
    
    made things (variables) simpler to re-use and adjust through one line of code rather than hundreds.

--- src/Game.java
+++ src/Game.java
@@ -55,46 +55,85 @@ public class Game{
 }
 
 
+class Clearing{
+    private Scanner scanner = new Scanner(System.in);
+    // turn is declared as negative 1 so that the turns only start counting when the first dealing is done. 
+    private int Turn = -1;
+    //Method that makes it so the player has to type "next" to hide his cards (clear the console), then the other player must type "next" to show their own cards (the next player in rotation).
+    public void clearing(){
 
+        String next = null;
+        next = scanner.nextLine();
+        //checks if console input was "clear"
+            if(next.equals("next")){
+                //clears console
+                System.out.print("\033[H\033[2J");
+                System.out.flush();
+
+                // adds 1 to the turn to let the next player know what turn they are on.
+                Turn++;
+                next = null;
+                }
+        next = scanner.nextLine();
+            if(next.equals("next")){
+                
+                if(Turn >=1){
+
+                    System.out.println("Turn #" + Turn);
+                }
+
+                return;
+            }
+        
+}
+}
 
 
 
 
 class Deck{
     public static void cards() {
+
+
+        //declaring the clearing method
+        Clearing clearer = new Clearing();
+
+    //memory numbers declared. These are variables so we can change the numbers if we need to. (If we want to add more players, or we need another variable for whatever reason)
+    int p1 = 1; // player 1's hand
+    int p2 = 2; // player 2's hand
+    int top = 3; // top of pile (last card(s) played)
+    int use = 4; // used pile (last 10 cards played)
+    int sp = 5; // spare pile
+
+
+
         int[][] card = new int[4][];
         
         card[0] = new int[25]; // Red
         
         for (int i = 0; i < 25; i++){
-            card[0][i] = 4; // Give each card a value of 4, which means it is in the deck
+            card[0][i] = sp; // Give each card a value of 4, which means it is in the deck
             }
 
         card[1] = new int[25]; // Blue
         
         for (int i = 0; i < 25; i++){
-            card[1][i] = 4; // Give each card a value of 4, which means it is in the deck
+            card[1][i] = sp; // Give each card a value of 4, which means it is in the deck
         }
 
         card[2] = new int[25]; // Green
         
         for (int i = 0; i < 25; i++){
-            card[2][i] = 4; // Give each card a value of 4, which means it is in the deck
+            card[2][i] = sp; // Give each card a value of 4, which means it is in the deck
         }
 
         card[3] = new int[8]; // Wild
         
         for (int i = 0; i < 8; i++){
-            card[3][i] = 4; // Give each card a value of 4, which means it is in the deck
+            card[3][i] = sp; // Give each card a value of 4, which means it is in the deck
         }
 
-        String[][] cardNames = new String[4][];
-
-        cardNames[0] = new String[25]; // Red
-
-        for (int i = 0; i < 25; i++){
-            cardNames[0][i] = "Red " + i; // Give each card a name of Red 0-12 (every card has a duplicate, except for 0)
-        }
+    
 
         
 
@@ -103,7 +142,7 @@ class Deck{
         
         String colour = null;
         
-        //card dealing
+        //card dealing for player 1
         int i = 0;
         for(i = 0; i < 7; i++){
             
@@ -117,7 +156,7 @@ class Deck{
                 index2 = (int)(Math.random() * 25); // The second index is the number of the card (0-25)
             }
 
-            if(card[index1][index2] == 4){
+            if(card[index1][index2] == sp){
 
                 if(index1 == 0){
                     colour = "red";
@@ -136,7 +175,7 @@ class Deck{
                 }
                 
                
-                card[index1][index2] = 1; // Give the card a value of one, which means it is in the player's hand
+                card[index1][index2] = p1; // Give the card a value of one, which means it is in player one's hand now.
                 System.out.println(colour + ' ' + index2);
             }
             else{
@@ -144,8 +183,52 @@ class Deck{
             }
         }
 
+        //clear console to rotate to player 2
+        clearer.clearing();
 
-           
+        // card dealing for player 2
+
+        for(i = 0; i < 7; i++){
+            
+            index1 = (int)(Math.random() * 4); // The first index is the color of the card (red, blue, green, or wild)
+            System.out.println(index1);
+
+            if (index1 == 3){ // If the card is wild (index1 == 3), the randomizer will draw from a pile of 8 cards
+                index2 = (int)(Math.random() * 8); // The wild card has a pile of 8 cards
+            }
+            else{
+                index2 = (int)(Math.random() * 25); // The second index is the number of the card (0-25)
+            }
+
+            if(card[index1][index2] == sp){
+
+                if(index1 == 0){
+                    colour = "red";
+                }
+
+                if(index1 == 1){
+                   colour = "blue";
+                }
+
+                if(index1 == 2){
+                    colour = "green";
+                }
+
+                if(index1 == 3){
+                    colour = "wild";
+                }
+                
+               
+                card[index1][index2] = p2; // Give the card a value of one, which means it is in player one's hand now.
+                System.out.println(colour + ' ' + index2);
+            }
+            else{
+                i--;
+            }
+        }
+
+        // using the clear tool.
+        clearer.clearing();
 
 
         }
